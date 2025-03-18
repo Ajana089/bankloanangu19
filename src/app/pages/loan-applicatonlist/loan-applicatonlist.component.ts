f4dbf593df1825f892959dbd174a2985e2d45ab6
Ajana Antony <ajanaantony089@gmail.com>
2025-03-18 17:47:53 +0400

    changes

---
+++ src/app/pages/loan-applicatonlist/loan-applicatonlist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-applicatonlist',
+  imports: [],
+  templateUrl: './loan-applicatonlist.component.html',
+  styleUrl: './loan-applicatonlist.component.css'
+})
+export class LoanApplicatonlistComponent {
+
+}
